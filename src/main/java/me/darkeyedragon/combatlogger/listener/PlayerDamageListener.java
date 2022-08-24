4c95f7467f7525b53a3265387172d50593f38f4d
DarkEyeDragon <mccrafter4liver@gmail.com>
2022-08-24 22:16:44 +0200

    Initial commit

---
+++ src/main/java/me/darkeyedragon/combatlogger/listener/PlayerDamageListener.java
@@ -0,0 +1,12 @@
+package me.darkeyedragon.combatlogger.listener;
+
+import org.bukkit.event.Listener;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+
+public class PlayerDamageListener implements Listener {
+
+    public void onPlayerDamage(EntityDamageByEntityEvent event){
+        //if()
+    }
+
+}
